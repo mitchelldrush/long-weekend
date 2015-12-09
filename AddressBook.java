7fb0caccb8c9c852fc4226eab52b3c99aad41895
jessebean <jessehmcdonald@gmail.com>
2015-12-09 17:24:27 -0500

    AddressBook.java
    The Main

---
+++ AddressBook.java
@@ -0,0 +1,17 @@
+
+public class AddressBook {
+
+	private String[][] addBook=new String[500][9];
+	private String[][] addBook2=new String[500][9];
+	
+	
+	public static void main(String[] args)
+	{
+		MainGUI main = new MainGUI();		
+	}
+
+	public void finalize()
+	{
+		
+	}
+}
